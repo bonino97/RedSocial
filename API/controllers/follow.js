ca39484ed3b4a93f15c224a1fb9a63c38ba8eae0
bonino97 <juanbonino97@gmail.com>
2019-08-29 17:34:13 -0300

    Funciones en instancia follow.
    Nuevas funcionalidades, metodos:
        saveFollow,
        deleteFollow,
        getFollowingUsers,
        getFollowedUsers,
        getMyFollows.

--- API/controllers/follow.js
+++ API/controllers/follow.js
@@ -21,6 +21,99 @@ function saveFollow(req,res){
     });
 }
 
+function deleteFollow(req,res){
+    var userId = req.user.sub;
+    var followId = req.params.id;
+
+    Follow.findOneAndRemove({'user':userId, 'followed': followId},err => {
+        if(err) return res.status(500).send({message: 'Error al dejar de seguir este usuario.'});
+        return res.status(200).send({message: 'Has dejado de seguir a este usuario.'});
+    });
+
+}
+
+//SIGUIENDO -- PAGINADOS
+
+function getFollowingUsers(req,res){
+    var userId = req.user.sub;
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+    if(req.params.page){
+        page = req.params.page;
+    }
+    else{
+        page = req.params.id;
+    }    
+
+    var itemsPerPage = 4;
+
+    Follow.find({user:userId}).populate({path:'followed'}).paginate(page,itemsPerPage,(err,follows,total)=>{
+        if(err) return res.status(500).send({message: 'Error en el servidor.'});
+        if(!follows) return res.status(404).send({message: 'No sigues a ningun usuario.'});
+        return res.status(200).send({
+            total:total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
+//SEGUIDOS -- PAGINADOS
+
+function getFollowedUsers(req,res){
+    var userId = req.user.sub;
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+    if(req.params.page){
+        page = req.params.page;
+    }
+    else{
+        page = req.params.id;
+    }    
+
+    var itemsPerPage = 4;
+
+    Follow.find({followed:userId}).populate('user followed').paginate(page,itemsPerPage,(err,follows,total)=>{
+        if(err) return res.status(500).send({message: 'Error en el servidor.'});
+        if(!follows) return res.status(404).send({message: 'No tienes seguidores.'});
+        return res.status(200).send({
+            total:total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
+//DEVOLVER USUARIOS QUE SIGO O ME SIGUEN.
+/*SIN PARAMETRO (SOLO TOKEN) LISTA LOS USUARIOS QUE SIGO.
+  SI COMO PARAMETRO LE PASO TRUE LISTA LOS USUARIOS QUE ME SIGUEN. 
+  (FALTA ANALIZAR UN POCO MAS - TODAVIA NO TERMINO DE COMPRENDER LA LOGICA.) -- VIDEO 34 */
+
+function getMyFollows(req,res){
+    var userId = req.user.sub;
+    var find = Follow.find({user:userId});
+
+    if(req.params.followed){
+        find = Follow.find({followed:userId});
+    }
+ 
+    find.populate('user followed').exec((err,follows) => {
+        if(err) return res.status(500).send({message: 'Error en el servidor.'});
+        if(!follows) return res.status(404).send({message: 'No sigues a ningun usuario.'});
+        return res.status(200).send({follows});
+    });
+}
+
 module.exports = {
-    saveFollow
+    saveFollow,
+    deleteFollow,
+    getFollowingUsers,
+    getFollowedUsers,
+    getMyFollows
 }
--- API/routes/user.js
+++ API/routes/user.js
@@ -14,13 +14,16 @@ var md_upload = multiParty({uploadDir:'./uploads/users'});
 
 api.get('/home', UserController.home);
 api.get('/pruebas',md_auth.ensureAuth, UserController.pruebas);
-api.post('/register',UserController.saveUser);
-api.post('/login',UserController.loginUser);
 api.get('/user/:id',md_auth.ensureAuth,UserController.getUser);
 api.get('/users/:page?',md_auth.ensureAuth,UserController.getUsers);
-api.put('/update-user/:id',md_auth.ensureAuth,UserController.updateUser);
-api.post('/upload-image-user/:id',[md_auth.ensureAuth,md_upload],UserController.uploadImage);
 api.get('/get-image-user/:imageFile',UserController.getImageFile);
+
+api.post('/register',UserController.saveUser);
+api.post('/login',UserController.loginUser);
+api.post('/upload-image-user/:id',[md_auth.ensureAuth,md_upload],UserController.uploadImage);
+
+api.put('/update-user/:id',md_auth.ensureAuth,UserController.updateUser);
+
 
 
 module.exports = api; 
