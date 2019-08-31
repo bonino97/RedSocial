36d55028362e78d4cb0a688930612bedb73758ef
bonino97 <juanbonino97@gmail.com>
2019-08-31 17:02:06 -0300

    Update app.js
    Se agregaron los CORS.
    
    Recurso: https://victorroblesweb.es/2017/11/09/configurar-cabeceras-acceso-cors-en-nodejs/

--- API/app.js
+++ API/app.js
@@ -19,6 +19,15 @@ app.use(bodyParser.json());
 
 // CORS
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
+
 // RUTAS
 
 app.use('/api', user_routes);
